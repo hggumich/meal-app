f2ea63d2a8bbc9b83e63b20a5b8330d32c04b96e
Hector Garza <hggumich@gmail.com>
2022-09-05 22:29:02 -0400

    install axios

--- src/context.jsx
+++ src/context.jsx
@@ -1,8 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+
+import axios from 'axios';
 
 const AppContext = React.createContext();
 
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+const fetchMeals = async (url) => {
+  try {
+    const { data } = await axios(url);
+
+    console.log(data);
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
 const AppProvider = ({ children }) => {
+  useEffect(() => {
+    fetchMeals(allMealsUrl);
+  }, []);
+
   return (
     <AppContext.Provider value={{ name: 'John', role: 'student' }}>
       {children}
