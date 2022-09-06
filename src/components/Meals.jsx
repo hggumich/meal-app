79705a46384674306284c6d840bc37f69217bfac
Hector Garza <hggumich@gmail.com>
2022-09-05 21:39:31 -0400

    setup a global app context

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import { useGlobalContext } from '../context';
 
 const Meals = () => {
+  const context = useGlobalContext();
+  console.log(context);
   return <div>Meals</div>;
 };
 
